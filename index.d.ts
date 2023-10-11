309dc595c4489bb6d77f7f822102b0f966adbe7b
FredTsang <17154608+FredZeng@users.noreply.github.com>
2023-10-11 20:40:01 +0800

    chore: update .gitignore

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,14 @@ export default class Peer {
   static WEBRTC_SUPPORT: boolean;
   static config: any;
   static channelConfig: any;
+
+  constructor(opts?: any);
+
+  public readonly _id: string;
+
+  get bufferSize(): number;
+
+  get connected(): boolean;
+
+  address(): { port?: number; family?: string; address?: string };
 }
