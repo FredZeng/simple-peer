56493409aa30a6b86d2b9b88be01b5f904fee8ff
FredTsang <17154608+FredZeng@users.noreply.github.com>
2023-10-12 10:48:53 +0800

    feat: update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,70 @@
-export default class Peer {
-  static WEBRTC_SUPPORT: boolean;
-  static config: any;
-  static channelConfig: any;
+import stream from 'readable-stream';
 
-  constructor(opts?: any);
+interface PeerOpts {
+  /**
+   * @default false
+   */
+  allowHalfOpen?: boolean;
+  /**
+   * @default false
+   */
+  initiator?: boolean;
+  /**
+   * @default null
+   */
+  channelName?: string;
+  /**
+   * custom webrtc data channel configuration (used by createDataChannel)
+   * @default {}
+   */
+  channelConfig?: RTCDataChannelInit;
+  /**
+   * custom webrtc configuration (used by RTCPeerConnection constructor)
+   */
+  config?: RTCConfiguration;
+  /**
+   * custom offer options (used by createOffer method)
+   * @default {}
+   */
+  offerOptions?: RTCOfferOptions;
+  /**
+   * custom answer options (used by createAnswer method)
+   * @default {}
+   */
+  answerOptions?: RTCAnswerOptions;
+  sdpTransform?: (sdp: string) => string;
+  stream?: any;
+  streams?: any;
+  /**
+   * @default true
+   */
+  trickle?: boolean;
+  wrtc?: any;
+  objectMode?: boolean;
+  /**
+   * @default 5000
+   */
+  iceCompleteTimeout?: number;
+}
+
+export default class Peer extends stream.Duplex {
+  static WEBRTC_SUPPORT: boolean;
+  static config: RTCConfiguration;
+  static channelConfig: RTCDataChannelInit;
 
   public readonly _id: string;
+  public channelName: string;
+  public readonly initiator: boolean;
+  private channelConfig: RTCDataChannelInit;
+  private channelNegotiated: boolean;
+  private config: RTCConfiguration;
+  private offerOptions: RTCOfferOptions;
+  private answerOptions: RTCAnswerOptions;
+  private sdpTransform: (sdp: string) => string;
+
+  private _pc: RTCPeerConnection;
+
+  constructor(opts?: PeerOpts);
 
   get bufferSize(): number;
 
